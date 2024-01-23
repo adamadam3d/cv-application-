71a823685ebe2b351b6bc6b0945bdf3312a2da76
adamadam3d <59068057+adamadam3d@users.noreply.github.com>
2024-01-23 11:11:50 +0000

    layout of Experience set

---
+++ src/components/Exp.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+export default function Exp() {
+
+    return(
+        <div>
+            <header>heading<button>arrow</button></header>
+            <form >
+                      
+                <label htmlFor="company">Company Name</label>
+                <input type="text" id='company'/>
+                <label htmlFor=""></label>
+                <input type="text" id='' />
+                <label htmlFor=""></label>
+                <input type="text" id=''/>
+                <label htmlFor=""></label>
+                <input type='text' id=''/>
+                
+            </form>
+        </div>
+    )
+}
